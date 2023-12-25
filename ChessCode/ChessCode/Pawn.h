ca140cb5738a7ea0506b302530c121b6c26a7b0b
Gilad Kupfer <giladkupfer@gmail.com>
2023-12-25 23:50:14 +0200

    removed accidental closing bracket

--- ChessCode/ChessCode/Pawn.h
+++ ChessCode/ChessCode/Pawn.h
@@ -11,5 +11,4 @@ private:
 	virtual bool arePiecesInWay(Piece* board[BOARD_ROWS][BOARD_COLS]) override;
 	virtual bool isDstInPath(Coord dst, Piece* board[BOARD_ROWS][BOARD_COLS]) override;
 };
-};
 
