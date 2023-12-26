d7a6ced9b18c7c0587fa978a266c236ea0960c60
Gilad Kupfer <giladkupfer@gmail.com>
2023-12-26 22:25:49 +0200

    finished rook

--- ChessCode/ChessCode/Rook.cpp
+++ ChessCode/ChessCode/Rook.cpp
@@ -1,5 +1,65 @@
 #include "Rook.h"
 
-Rook::Rook(Coord coord, int color) : Piece(coord, color)
+
+Rook::Rook(Coord coord, Color color) : Piece(coord, color)
 {
+	if (this->_color == WHITE_PIECE) // if the piece is white
+	{
+		this->_sign = WhiteRook; // set the sign to a white rook
+	}
+	else // piece is black
+	{
+		this->_sign = BlackRook; // set the sign to a black rook
+	}
+}
+
+MoveResult Rook::checkMove(Coord dst, Piece* board[BOARD_ROWS][BOARD_COLS])
+{
+	
+	auto dstInPathResult = isDstInPath(dst); // store the returned tuple
+	// extract the values
+	bool isInPath;
+	Direction direction;
+	std::tie(isInPath, direction) = dstInPathResult;
+
+	if (!isInPath) // if the destination is not in path
+	{
+		return IllegalPieceMove; // return illegal piece move
+	}
+
+	// check if there are pieces in the way that disallows the piece to move there
+	if (arePiecesInWay(board, direction, dst))
+	{
+		return IllegalPieceMove; // return illegal piece move
+	}
+
+	return Legal; // we got here meanign its a legal move
+}
+
+
+bool Rook::arePiecesInWay(Piece* board[BOARD_ROWS][BOARD_COLS], Direction direction, Coord dst)
+{
+	Coord currentCoord = calcDst(this->_coord, 1, direction); // save the next piece
+	while (!currentCoord.isEqual(dst)) // while we havent reached the dest yet
+	{
+		if (board[currentCoord.Row][currentCoord.Col] != NULL)
+		{
+			return true; // piece ARE in the way
+		}
+		currentCoord = calcDst(currentCoord, 1, direction); // go to the next coord
+	}
+	return false; // we got here meaning there were no pieces in the way
+}
+
+std::tuple<bool, Direction> Rook::isDstInPath(Coord dst)
+{
+	// the movement should be in a direct line,
+	// meaning that either the row (or the col) of the source should be equal to row (or the col) of the dest
+	if (this->_coord.Row != dst.Row && this->_coord.Col != dst.Col)
+	{
+		return std::make_tuple(false, None); // return false and garbage char
+	}
+
+	return std::make_tuple(true, getDirection(this->_coord, dst)); // return true and the direction
+	
 }
--- ChessCode/ChessCode/Rook.h
+++ ChessCode/ChessCode/Rook.h
@@ -4,11 +4,10 @@
 class Rook : public Piece
 {
 public:
-	Rook(Coord coord, int color);
-	virtual char checkMove(Coord dst, Piece* board[BOARD_ROWS][BOARD_COLS]) override;
+	Rook(Coord coord, Color color);
+	virtual MoveResult checkMove(Coord dst, Piece* board[BOARD_ROWS][BOARD_COLS]) override;
 
 private:
-	virtual bool arePiecesInWay(Piece* board[BOARD_ROWS][BOARD_COLS]) override;
-	virtual bool isDstInPath(Coord dst, Piece* board[BOARD_ROWS][BOARD_COLS]) override;
-	bool fastValidCheck(Coord dst);
+	virtual bool arePiecesInWay(Piece* board[BOARD_ROWS][BOARD_COLS], Direction direction, Coord dst) override;
+	virtual std::tuple<bool, Direction> isDstInPath(Coord dst) override;
 };
