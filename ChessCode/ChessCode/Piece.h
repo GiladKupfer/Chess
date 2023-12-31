beea495b229776221dfd530192b7238dae95e141
Gilad Kupfer <giladkupfer@gmail.com>
2023-12-31 20:21:40 +0200

    made the direction enum numbers, made calcDst public

--- ChessCode/ChessCode/Piece.h
+++ ChessCode/ChessCode/Piece.h
@@ -52,21 +52,25 @@ enum MoveResult
 	InvalidSquareIndexes = '5',
 	IllegalPieceMove = '6',
 	SourceAndTargetSquaresEqual = '7',
+
+	// checkmate
+	CheckMate = '8',
+
 };
 
 enum Direction
 {
-	None = '-',
+	None = -1,
 
-	UP = 'u',
-	DOWN = 'd',
-	LEFT = 'l',
-	RIGHT = 'r',
+	UP = 0,
+	DOWN,
+	LEFT,
+	RIGHT,
 
-	UP_RIGHT = 'ur',
-	UP_LEFT = 'ul',
-	DOWN_RIGHT = 'dr',
-	DOWN_LEFT = 'dl',
+	UP_RIGHT,
+	UP_LEFT,
+	DOWN_RIGHT,
+	DOWN_LEFT,
 };
 
 class Piece
@@ -82,12 +86,12 @@ protected:
 	std::tuple<int, int> calcSignedDelta(Coord coord1, Coord coord2);
 	virtual bool arePiecesInWay(Piece* board[BOARD_ROWS][BOARD_COLS], Direction direction, Coord dst) = 0;
 	virtual std::tuple<bool, Direction> isDstInPath(Coord dst) = 0;
-	static Coord calcDst(Coord src, int numOfSteps, Direction direction);
 	Direction getDirection(Coord src, Coord dst);
 
 public:
 	Piece(Coord coord, Color color);
 	virtual MoveResult checkMove(Coord dst, Piece* board[BOARD_ROWS][BOARD_COLS]) = 0;
+	static Coord calcDst(Coord src, int numOfSteps, Direction direction);
 	void setCoord(Coord coord);
 	Coord getCoord() const;
 	Color getColor() const;
